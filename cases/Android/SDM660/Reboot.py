2acb604a47450d69a7cc7e187514208b9b3c4d23
wuyou1102 <jotey@qq.com>
2019-01-07 19:55:20 +0800

    add

--- cases/Android/SDM660/Reboot.py
+++ cases/Android/SDM660/Reboot.py
@@ -132,95 +132,97 @@ class test_AR8020_Node(BaseCase.AndroidCase):
             self.On_CIT(on=False)
             self.On_8020(on=False)
             return self.ResultPass
-        while True:
-            self.sleep(10)
+
         self.On_CIT(on=False)
         self.On_8020(on=False)
-        return self.ResultFail
-
-
-class test_AR8020_Node_For_DVT(BaseCase.AndroidCase):
-    name = u"检查AR8020节点和UART(660为DVT)"
-
-    def __init__(self, device):
-        BaseCase.AndroidCase.__init__(self)
-        self.device = device
-        Utility.execute_command(command=Command.adb.root(serial=self.device))
-        self.sleep(0.5)
-        Utility.execute_command(command=Command.adb.wait_for_device(serial=self.device))
-        self.On_8020(on=False)
-
-    def BeforeTest(self):
-        Utility.execute_command(Command.adb.shell_command(cmd="logcat -c", serial=self.device))
-
-    def On_8020(self, on=True):
-        on = '1' if on else '0'
-        Utility.execute_command(
-            Command.adb.shell_command(cmd='echo %s > /sys/bus/platform/drivers/artosyn_ar8020/soc:ar8020/enable' % on,
-                                      serial=self.device))
-
-    def On_CIT(self, on=True):
-        on = '1' if on else '0'
-        Utility.execute_command(
-            Command.adb.shell_command(cmd='setprop persist.data.uartlog.cit %s' % on, serial=self.device))
-        result = Utility.execute_command(
-            Command.adb.shell_command(cmd='getprop persist.data.uartlog.cit', serial=self.device))
-        self.Debug(u"检查UART CIT打开情况：%s" % result.outputs)
-
-    def AssertUart(self):
-        self.Debug(u"打开UART CIT测试，并等待2秒")
-        self.On_CIT(on=True)
-        self.sleep(2)
-        exec_rslt = Utility.execute_command(
-            Command.adb.shell_command(cmd='getprop persist.data.uartlog', serial=self.device))
-        self.Debug('getprop <uartlog> ---> %s' % (exec_rslt.outputs))
-        if exec_rslt.exit_code == 0 and '1' in exec_rslt.outputs:
-            return True
-        self.Error("Uart测试失败:Expect ['1'] , But was %s" % exec_rslt.outputs)
-        t = Utility.execute_command(command=Command.adb.shell_command("logcat -d", serial=self.device))
-        for line in t.outputs:
+        wifi = Utility.execute_command(command=Command.adb.shell_command("ifconfig", serial=self.device))
+        for line in wifi.outputs:
             self.Info(line)
-        return False
-
-    def AssertNode(self):
-        result = True
-        self.Debug(u"打开AR8020，并等待4秒")
-        self.On_8020(on=True)
-        self.sleep(4)
-        self.Debug(u"开始检查AR8020节点")
-        for x in range(1, 21):
-            exec_rslt = Utility.execute_command(
-                command=Command.adb.shell_command(
-                    'ls /sys/devices/soc/c200000.hsusb/c200000.dwc3/xhci-hcd.0.auto/usb1/1-1/ | grep 1-1 ',
-                    serial=self.device))
-            self.Debug(u"第%s次结果：%s" % (x, exec_rslt.outputs))
-            if exec_rslt.exit_code == 0 and len(exec_rslt.outputs) == 4:
-                break
-            self.sleep(0.5)
-        for x in range(4):
-            if "1-1:1.%s" % x in exec_rslt.outputs:
-                result = result and True
-            else:
-                self.Error(u"无法找到：1.%s" % x)
-                result = result and False
-        return result
-
-    def Test(self):
-        node_result = self.AssertNode()
-        uart_result = True  # self.AssertUart()
-        Utility.execute_command(Command.adb.shell_command('setprop persist.data.uartlog 0', serial=self.device))
-        self.Debug('重新设置setprop <uartlog> <--- 0')
-        exec_rslt = Utility.execute_command(
-            Command.adb.shell_command(cmd='getprop persist.data.uartlog', serial=self.device))
-        self.Debug('重新获取getprop <uartlog> ---> %s' % (exec_rslt.outputs))
-        self.On_CIT(on=False)
-        self.Debug(u"关闭AR8020 和 UART CIT")
-        if node_result and uart_result:
-            self.On_8020(on=False)
-            return self.ResultPass
-        while True:
-            self.sleep(10)
         return self.ResultFail
+
+
+# class test_AR8020_Node_For_DVT(BaseCase.AndroidCase):
+#     name = u"检查AR8020节点和UART(660为DVT)"
+#
+#     def __init__(self, device):
+#         BaseCase.AndroidCase.__init__(self)
+#         self.device = device
+#         Utility.execute_command(command=Command.adb.root(serial=self.device))
+#         self.sleep(0.5)
+#         Utility.execute_command(command=Command.adb.wait_for_device(serial=self.device))
+#         self.On_8020(on=False)
+#
+#     def BeforeTest(self):
+#         Utility.execute_command(Command.adb.shell_command(cmd="logcat -c", serial=self.device))
+#
+#     def On_8020(self, on=True):
+#         on = '1' if on else '0'
+#         Utility.execute_command(
+#             Command.adb.shell_command(cmd='echo %s > /sys/bus/platform/drivers/artosyn_ar8020/soc:ar8020/enable' % on,
+#                                       serial=self.device))
+#
+#     def On_CIT(self, on=True):
+#         on = '1' if on else '0'
+#         Utility.execute_command(
+#             Command.adb.shell_command(cmd='setprop persist.data.uartlog.cit %s' % on, serial=self.device))
+#         result = Utility.execute_command(
+#             Command.adb.shell_command(cmd='getprop persist.data.uartlog.cit', serial=self.device))
+#         self.Debug(u"检查UART CIT打开情况：%s" % result.outputs)
+#
+#     def AssertUart(self):
+#         self.Debug(u"打开UART CIT测试，并等待2秒")
+#         self.On_CIT(on=True)
+#         self.sleep(2)
+#         exec_rslt = Utility.execute_command(
+#             Command.adb.shell_command(cmd='getprop persist.data.uartlog', serial=self.device))
+#         self.Debug('getprop <uartlog> ---> %s' % (exec_rslt.outputs))
+#         if exec_rslt.exit_code == 0 and '1' in exec_rslt.outputs:
+#             return True
+#         self.Error("Uart测试失败:Expect ['1'] , But was %s" % exec_rslt.outputs)
+#         t = Utility.execute_command(command=Command.adb.shell_command("logcat -d", serial=self.device))
+#         for line in t.outputs:
+#             self.Info(line)
+#         return False
+#
+#     def AssertNode(self):
+#         result = True
+#         self.Debug(u"打开AR8020，并等待4秒")
+#         self.On_8020(on=True)
+#         self.sleep(4)
+#         self.Debug(u"开始检查AR8020节点")
+#         for x in range(1, 21):
+#             exec_rslt = Utility.execute_command(
+#                 command=Command.adb.shell_command(
+#                     'ls /sys/devices/soc/c200000.hsusb/c200000.dwc3/xhci-hcd.0.auto/usb1/1-1/ | grep 1-1 ',
+#                     serial=self.device))
+#             self.Debug(u"第%s次结果：%s" % (x, exec_rslt.outputs))
+#             if exec_rslt.exit_code == 0 and len(exec_rslt.outputs) == 4:
+#                 break
+#             self.sleep(0.5)
+#         for x in range(4):
+#             if "1-1:1.%s" % x in exec_rslt.outputs:
+#                 result = result and True
+#             else:
+#                 self.Error(u"无法找到：1.%s" % x)
+#                 result = result and False
+#         return result
+#
+#     def Test(self):
+#         node_result = self.AssertNode()
+#         uart_result = True  # self.AssertUart()
+#         Utility.execute_command(Command.adb.shell_command('setprop persist.data.uartlog 0', serial=self.device))
+#         self.Debug('重新设置setprop <uartlog> <--- 0')
+#         exec_rslt = Utility.execute_command(
+#             Command.adb.shell_command(cmd='getprop persist.data.uartlog', serial=self.device))
+#         self.Debug('重新获取getprop <uartlog> ---> %s' % (exec_rslt.outputs))
+#         self.On_CIT(on=False)
+#         self.Debug(u"关闭AR8020 和 UART CIT")
+#         if node_result and uart_result:
+#             self.On_8020(on=False)
+#             return self.ResultPass
+#         while True:
+#             self.sleep(10)
+#         return self.ResultFail
 
 
 if __name__ == '__main__':
--- libs/UserInterface/Dialogs/CreateCase/_4_Loop.py
+++ libs/UserInterface/Dialogs/CreateCase/_4_Loop.py
@@ -6,5 +6,5 @@ from libs.Config import String
 
 class LoopSelection(Base.IntSettingPage):
     def __init__(self, parent):
-        Base.IntSettingPage.__init__(self, parent=parent, attr_name=String.CaseLoop, min=0, max=10000, initial=100,
+        Base.IntSettingPage.__init__(self, parent=parent, attr_name=String.CaseLoop, min=0, max=10000, initial=0,
                                      title=u"请输入循环次数(0为无穷): ")
